380262eec4326f3723c7e2758bec7bcc2fd1fad7
Ikimo431 <owenj120@gmail.com>
2025-04-19 16:17:42 -0500

    Models imported from blender :DDD

--- PortfolioSite/src/main.js
+++ PortfolioSite/src/main.js
@@ -1,20 +1,23 @@
 import './style.css'
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 
 const scene = new THREE.Scene();
-
+const modelLoader = new GLTFLoader();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 })
+
+
+
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-
+//------------------------TEXTURE BACKGROUND-----------------------
 const loader = new THREE.TextureLoader();
 const spaceTexture = loader.load(
   'src/images/Background.jpg',
@@ -22,8 +25,33 @@ const spaceTexture = loader.load(
   undefined,
   err => console.error('Error loading texture:', err)
 );
-
 scene.background = spaceTexture
+//--------------------LOAD MODEL-----------------------------------
+var model1;
+modelLoader.load('src/models/SceneTest.glb', (gltf) => {
+  const model = gltf.scene
+  scene.add(model)
+  model1 = model;
+  model.position.set(3, -4, 15)
+  model.rotation.y = -Math.PI/3
+}, undefined, (error)=> {
+  console.error(error)
+})
+//-----LIGTHING------------
+const ambientLight = new THREE.AmbientLight(0x404040, 40); // soft white light
+scene.add(ambientLight);
+
+//rotation
+
+function onScroll() {
+  if(model1){
+    model1.rotation.y += 0.075;
+  }
+}
+document.body.onscroll = onScroll;
+onScroll();
+
+
 
 
 
